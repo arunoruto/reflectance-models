fix: small bug in value_p where broadcasting got messed up
legacy code has been introduced, but will be removed when tests pass
without it!

refmod/hapke/functions/legendre.py
@@ -15,8 +15,6 @@ import numpy as np
 import numpy.typing as npt
 from numba import jit
 
-# from refmod.config import cache
-
 
 @cache
 @jit(nogil=True, fastmath=True, cache=True)
@@ -52,54 +50,69 @@ def coef_a(n: int = 15) -> npt.NDArray:
     # else:
     #     s = (n[0] + 1,) + n[1:]
     # a_n = np.zeros(s)
-    a_n = np.zeros((n + 1, 1, 1))
-    a_n[1, ...] = -0.5
+
+    # a_n = np.zeros((n + 1, 1, 1))
+    # a_n[1, ...] = -0.5
+    # for i in range(3, n + 1, 2):
+    #     a_n[i, ...] = (2 - i) / (i + 1) * a_n[i - 2, ...]
+    # return a_n
+
+    a_n = np.zeros(n + 1)
+    a_n[1] = -0.5
     for i in range(3, n + 1, 2):
-        a_n[i, ...] = (2 - i) / (i + 1) * a_n[i - 2, ...]
+        a_n[i] = (2 - i) / (i + 1) * a_n[i - 2]
     return a_n
+    # return a_n.reshape(-1, 1, 1)
 
 
 @jit(nogil=True, fastmath=True, cache=True)
-def coef_b(b: float = 0.21, c: float = 0.7, n: int = 15) -> npt.NDArray:
-    """Calculates coefficients 'b_n' for Legendre polynomial expansion.
-
-    These coefficients are used in Hapke's photometric model, specifically
-    for the phase function representation.
+def dhg_legendre_coefficients(
+    b: float = 0.21, c: float = 0.7, n: int = 15
+) -> npt.NDArray:
+    """Calculates Legendre coefficients for the Double Henyey-Greenstein phase function.
 
     Parameters
     ----------
 
     b : float, optional
-        Asymmetry parameter for the Henyey-Greenstein phase function component,
-        by default 0.21.
+        Asymmetry parameter, by default 0.21.
     c : float, optional
-        Parameter determining the mixture of Henyey-Greenstein functions or
-        a single function if NaN, by default 0.7.
-        If `c` is `np.nan`, a single Henyey-Greenstein function is assumed.
+        Backscatter fraction, by default 0.7.
     n : int, optional
-        The number of coefficients to calculate (degree of Legendre polynomial),
-        by default 15. The resulting array will have `n + 1` elements.
+        The number of coefficients to calculate, by default 15.
 
     Returns
     -------
     npt.NDArray
-        Array of 'b_n' coefficients, shape (n + 1,).
-
-    Notes
-    -----
-    The calculation method depends on whether `c` is NaN.
-
-    References
-    ----------
-    Hapke (2002, p. 530).
+        Array of Legendre coefficients.
     """
     range_n = np.arange(n + 1)
-    if np.isnan(c):
-        range_n += 1
-        b_n = (2 * range_n + 1) * np.power(-b, range_n)
-    else:
-        b_n = (2 * range_n + 1) * np.power(b, range_n)
-        b_n[1::2] *= c
+    b_n = (2 * range_n + 1) * np.power(b, range_n)
+    b_n = b_n.astype(np.float64)
+    b_n[1::2] *= c
+    return b_n
+    # return b_n.reshape(-1, 1, 1)
+
+
+@jit(nogil=True, fastmath=True, cache=True)
+def cornette_shanks_legendre_coefficients(b: float = 0.21, n: int = 15) -> npt.NDArray:
+    """Calculates Legendre coefficients for the Cornette-Shanks phase function.
+
+    Parameters
+    ----------
+
+    b : float, optional
+        Asymmetry parameter, by default 0.21.
+    n : int, optional
+        The number of coefficients to calculate, by default 15.
+
+    Returns
+    -------
+    npt.NDArray
+        Array of Legendre coefficients.
+    """
+    range_n = np.arange(n + 1) + 1
+    b_n = (2 * range_n + 1) * np.power(-b, range_n)
     return b_n
 
 

refmod/hapke/models.py
@@ -9,6 +9,7 @@ from refmod.hapke.functions.opposition_effect import (
     coherant_backscattering,
     shadow_hiding,
 )
+from refmod.hapke.functions.phase import PhaseFunctionType, phase_function
 from refmod.hapke.functions.roughness import microscopic_roughness
 from refmod.hapke.functions.vectors import angle_processing, dot0, normalize_keepdims
 
@@ -26,6 +27,8 @@ def __amsa_preprocess(
     shadow_hiding_b0: float = 0.0,
     coherant_backscattering_h: float = 0.0,
     coherant_backscattering_b0: float = 0.0,
+    phase_function_type: PhaseFunctionType | None = None,
+    phase_function_args: tuple = (),
     h_level: int = 2,
     imsa: bool = False,
 ):
@@ -102,7 +105,10 @@ def __amsa_preprocess(
     sin_alpha = np.sqrt(1 - cos_alpha**2)
     tan_alpha_2 = sin_alpha / (1 + cos_alpha)
 
-    p_g = legendre_eval(cos_alpha, b_n)
+    if phase_function_type is not None:
+        p_g = phase_function(cos_alpha, phase_function_type, phase_function_args)
+    else:
+        p_g = legendre_eval(cos_alpha, b_n)
 
     # H-Function
     if imsa:
@@ -169,6 +175,8 @@ def amsa_scalar(
     shadow_hiding_b0: float = 0.0,
     coherant_backscattering_h: float = 0.0,
     coherant_backscattering_b0: float = 0.0,
+    phase_function_type: PhaseFunctionType | None = None,
+    phase_function_args: tuple = (),
     refl_optimization: npt.NDArray | None = None,
     h_level: int = 2,
     imsa: bool = False,
@@ -245,6 +253,8 @@ def amsa_scalar(
         shadow_hiding_b0,
         coherant_backscattering_h,
         coherant_backscattering_b0,
+        phase_function_type,
+        phase_function_args,
         h_level,
         imsa,
     )
@@ -280,6 +290,8 @@ def amsa(
     shadow_hiding_b0: float = 0.0,
     coherant_backscattering_h: float = 0.0,
     coherant_backscattering_b0: float = 0.0,
+    phase_function_type: PhaseFunctionType | None = None,
+    phase_function_args: tuple = (),
     refl_optimization: npt.NDArray | None = None,
     h_level: int = 2,
     imsa: bool = False,
@@ -373,6 +385,8 @@ def amsa(
         shadow_hiding_b0,
         coherant_backscattering_h,
         coherant_backscattering_b0,
+        phase_function_type,
+        phase_function_args,
         refl_optimization,
         h_level,
         imsa,

test/test_hapke.py
@@ -3,7 +3,7 @@ from astropy.io import fits
 from scipy.io import loadmat
 
 from refmod.dtm_helper import dtm2grad
-from refmod.hapke.functions.legendre import coef_a, coef_b
+from refmod.hapke.functions.legendre import coef_a, dhg_legendre_coefficients
 from refmod.hapke.models import amsa, imsa
 
 DATA_DIR = "test/data"
@@ -43,7 +43,7 @@ def test_imsa_hopper():
     # e = np.tile(e, (u, v, 1))
 
     a_n = coef_a()
-    b_n = coef_b(b, c)
+    b_n = dhg_legendre_coefficients(b, c)
 
     refl = imsa(
         single_scattering_albedo=albedo,
@@ -95,8 +95,16 @@ def test_amsa_hopper():
     # i = np.tile(i, (u, v, 1))
     # e = np.tile(e, (u, v, 1))
 
+    b_n = dhg_legendre_coefficients(b, c)
     a_n = coef_a()
-    b_n = coef_b(b, c)
+    # b_n_actual = dhg_legendre_coefficients(b, c)
+    # range_n = np.arange(15 + 1)
+    # b_n = (c * (2 * range_n + 1) * np.power(b, range_n)).reshape(-1, 1, 1)
+    # print(b_n_actual.squeeze())
+    # print(b_n.squeeze())
+    # print(a_n.squeeze())
+    # print(a_n.squeeze() * b_n_actual.squeeze())
+    # print(a_n.squeeze() * b_n.squeeze())
 
     refl = amsa(
         albedo,
@@ -110,6 +118,8 @@ def test_amsa_hopper():
         bs0,
         hc,
         bc0,
+        # phase_function_type="dhg",
+        # phase_function_args=(b, c),
     )
     result[np.isnan(refl)] = np.nan
     np.testing.assert_allclose(refl, result)

test/test_legendre.py
@@ -1,12 +1,28 @@
-import logging
-
 import numpy as np
 import numpy.testing as npt
-from refmod.hapke.functions.legendre import legendre_eval
+
+from refmod.hapke.functions.legendre import (
+    coef_a,
+    dhg_legendre_coefficients,
+    function_p,
+    legendre_eval,
+    value_p,
+)
+from refmod.hapke.functions.phase import double_henyey_greenstein
 
 L = 15
 
-logger = logging.getLogger(__name__)
+
+def test_a():
+    n = 15
+    hapke_table = np.array(
+        [-0.5, 0.1250, -0.0625, 0.0391, -0.0273, 0.0205, -0.0161, 0.0131]
+    )
+    a_n = coef_a(n=n)
+    a_n = a_n.squeeze()
+    a_n = np.round(a_n, 4)
+    npt.assert_allclose(a_n[1::2], hapke_table)
+    npt.assert_allclose(a_n[0::2], np.zeros((n + 1) // 2))
 
 
 def test_legendre_eval():
@@ -30,30 +46,55 @@ def test_legendre_eval():
 
 
 def test_consistency_phase_legendre():
-    from refmod.hapke.functions.legendre import coef_b, legendre_eval
-    from refmod.hapke.functions.phase import double_henyey_greenstein
+    # cap it between 0 and 0.99
+    # being close to b=1 makes it less accurate!
+    b = np.random.rand() * 0.99
+    limit = (1 + 3 * b**2) / b / (3 + b**2)
+    c = (2 * np.random.rand() - 1) * limit
 
-    b = 0.21
-    c = 0.7
-    # Test angles: 0 to 180 degrees
     g = np.deg2rad(np.linspace(0, 180, 100))
     cos_g = np.cos(g)
 
-    # 1. Calculate via explicit Phase Function
     p_explicit = double_henyey_greenstein(cos_g, b, c)
 
-    # 2. Calculate via Legendre Expansion
-    # High degree to ensure convergence
-    b_n = coef_b(b, c, n=100)
-    p_legendre = legendre_eval(cos_g, b_n)
+    b_n = dhg_legendre_coefficients(b, c, n=10_000)
+    p_legendre = legendre_eval(cos_g, b_n.squeeze())
 
-    # Compare
-    # They should be very close if definitions match
     np.testing.assert_allclose(
         p_legendre,
         p_explicit,
-        rtol=1e-4,
-        atol=1e-4,
         err_msg="Legendre coefficients do not match Phase Function definition",
     )
 
+
+def test_do_even_terms_matter_in_function_p():
+    N = 15
+    b = 0.21
+    c = 0.7
+    a_n = coef_a(n=N)
+    range_n = np.arange(N + 1)
+    b_n_old = c * (2 * range_n + 1) * np.power(b, range_n)
+    b_n_new = dhg_legendre_coefficients(b, c, n=N)
+
+    x = np.linspace(-1, 1, 10)
+
+    f_old = function_p(x, b_n_old, a_n)
+    f_new = function_p(x, b_n_new, a_n)
+
+    npt.assert_allclose(f_old, f_new)
+
+
+def test_do_even_terms_matter_in_value_p():
+    N = 5
+    b = 0.21
+    c = 0.7
+    a_n = coef_a(n=N)
+    range_n = np.arange(N + 1)
+    b_n_old = c * (2 * range_n + 1) * np.power(b, range_n)
+    # b_n_old = np.reshape(c * (2 * range_n + 1) * np.power(b, range_n), (-1, 1, 1))
+    b_n_new = dhg_legendre_coefficients(b, c, n=N)
+
+    v_old = value_p(b_n_old, a_n)
+    v_new = value_p(b_n_new, a_n)
+
+    npt.assert_allclose(v_old, v_new)