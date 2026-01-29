bf58386d777ad1fdd2525d88584a66e44d2e3a5f
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-01-29 17:32:21 +0100

    ci: modernize the semantic release with uv

--- refmod/__init__.py
+++ refmod/__init__.py
@@ -1,5 +1,8 @@
+from importlib.metadata import version as get_version
+
 from .hapke import Hapke
 
 __all__ = ["Hapke"]
 
-__version__ = "0.1.0"
+__version__ = get_version(__package__) if __package__ else "0.0.0"
+# __version__ = "0.1.0"
